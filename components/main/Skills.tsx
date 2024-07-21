a0ba4a2ec50db86e2517cc56b12197c99a2b84e6
ashtonliu88 <130876023+ashtonliu88@users.noreply.github.com>
2024-07-21 16:16:31 -0700

    Updated so video working

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -66,7 +66,7 @@ const Skills = () => {
         ))}
       </div>
 
-      <div className="w-full h-full abs">
+      <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
           <video
             className="w-full h-auto"
