18187f3b04e36345a1c58b3b37ffb594b191c1c6
ashtonliu88 <130876023+ashtonliu88@users.noreply.github.com>
2024-07-25 15:36:22 -0700

    comments :)

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -50,6 +50,7 @@ const HeroContent = () => {
         <motion.a
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[300px]"
+          //Work on fixing this part
           href="/assets/Ashton_Resume.pdf"
           target="_blank"
           rel="noopener noreferrer"
