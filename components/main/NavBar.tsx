3321285cda74d6069c0f180fd4710fb7dab3b832
Ashton Liu <130876023+ashtonliu88@users.noreply.github.com>
2024-09-14 21:28:46 -0700

    No apostrophe lol

--- components/main/NavBar.tsx
+++ components/main/NavBar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         >
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            Ashton Liu's Website
+            Ashton Liu
           </span>
         </a>
 
