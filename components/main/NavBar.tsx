d7e2732d9ecb11f9c11487d2c53c98fe1c0765fc
Ashton Liu <130876023+ashtonliu88@users.noreply.github.com>
2024-09-14 21:20:34 -0700

    Get rid of Webchain Dev feature

--- components/main/NavBar.tsx
+++ components/main/NavBar.tsx
@@ -10,16 +10,9 @@ const Navbar = () => {
           href="#about-me"
           className="h-auto w-auto flex flex-row items-center"
         >
-          <Image
-            src="/NavLogo.png"
-            alt="logo"
-            width={70}
-            height={70}
-            className="cursor-pointer hover:animate-slowspin"
-          />
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            WebChain Dev
+            Ashton Liu's Website
           </span>
         </a>
 
