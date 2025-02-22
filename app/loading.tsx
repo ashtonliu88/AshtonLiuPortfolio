3528c7735b7fd56b721a132c5def61b578dfe273
ascliu123 <130876023+ashtonliu88@users.noreply.github.com>
2025-02-21 21:59:20 -0800

    starting over

--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="container flex min-h-[calc(100vh-8rem)] items-center justify-center">
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
-    </div>
-  )
-}
-
